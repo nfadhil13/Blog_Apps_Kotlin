e9cec3e5ddbefbc566b50af89a15a57c19dc47aa
nfadhil13 <48423700+nfadhil13@users.noreply.github.com>
2020-07-29 14:37:13 +0700

    First

---
+++ Blog_App/Open-API-Android-App-Starting-Point/app/src/main/java/com/codingwithmitch/openapi/ui/auth/AuthActivity.kt
@@ -0,0 +1,14 @@
+package com.codingwithmitch.openapi.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.codingwithmitch.openapi.R
+
+class AuthActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
